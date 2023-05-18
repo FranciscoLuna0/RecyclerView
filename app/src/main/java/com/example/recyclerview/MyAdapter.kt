9ead5ef3b0babd909ee9e6ba4bdeb51537ece44a
Jose Francisco Luna <pacolunahdz@hotmail.com>
2023-05-17 19:53:24 -0600

    Quedamos en la diapositiva 12...

--- app/src/main/java/com/example/recyclerview/MyAdapter.kt
+++ app/src/main/java/com/example/recyclerview/MyAdapter.kt
@@ -1,4 +1,25 @@
 package com.example.recyclerview
 
-class MyAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.view.view
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter:RecyclerView.Adapter<MyAdapter.ViewHolder?>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
 }
